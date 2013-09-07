77b97e1497a3f03ce6748a2912d8a971a93830c9
Brian Han <brianhan87usa@gmail.com>
2013-09-07 12:46:02 -0400

    First commit; rails scaffold Picture; some css styling

---
+++ photogur_scaffold/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotogurScaffold::Application.config.secret_token = 'fd162d7c9d4dcc2112447418cb585d1ea61b9d982e9f3a5d2e7c6600b8ea5655441617395ca6391de8761998ae90f217e8feb342f10ce6947ff107ffd52c8a3c'
